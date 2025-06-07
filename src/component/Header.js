1f0435e5fbda612e70d6c919d2473fafff17376f
dishajasoliya <dishajasoliya90@gmail.com>
2025-06-07 11:53:54 +0530

    change

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,24 +1,3 @@
-// import React from 'react';
-// import './style.css';
-
-// const Header = () => (
-//   <nav className="header">
-//     <div   className="logo">
-//     <img src="/images/logo1.png" alt="Logo" class="logo" />
-//     </div>   
-    
-//       <ul>
-//         <li><a href="Home">Home</a></li>
-//         <li><a href="About">About</a></li>
-//         <li><a href="Product">Product</a></li>
-//         <li><a href="ContactUs">Contact</a></li>
-//       </ul>
-    
-//   </nav>
-// );
-
-// export default Header;
-
 "use client"
 
 import { useState } from "react"
