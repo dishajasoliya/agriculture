7760d55847ff0b6220a79db3c462e003feaaef1c
dishajasoliya <dishajasoliya90@gmail.com>
2025-06-09 16:03:24 +0530

    change

--- src/component/AboutSection.js
+++ src/component/AboutSection.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './style.css';
-// import aboutImg from '../assets/about.jpg'; // replace with your image
-// import carbonImg from './images/carbon.jpg';
+
 
 const AboutSection = () => (
   
